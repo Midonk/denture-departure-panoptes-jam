9289fb7e173290a2b8cc07fd55eb6b52c07cbdf4
DESKTOP-SRL43AG\Midonk <t_finet@hotmail.fr>
2021-02-19 22:11:26 +0100

    TPS cam

---
+++ jam-panoptes/Assets/Private/TF/TPSMovement camNchar/CharMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour
+{
+    private CharacterController cc;
+
+    private void Awake() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
